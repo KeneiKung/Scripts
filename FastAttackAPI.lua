073d104c4107eca9b7b614bc9e0eab09c8dac91a
KeneiKung <132286409+KeneiKung@users.noreply.github.com>
2023-05-12 11:52:25 +0700

    Update FastAttackAPI.lua

--- FastAttackAPI.lua
+++ FastAttackAPI.lua
@@ -1,69 +1,72 @@
 getgenv().Config = {
-    FastAttack = true
+	FastAttack = true
 }
-local plr = game.Players.LocalPlayer
-local CbFw = debug.getupvalues(require(plr.PlayerScripts.CombatFramework))
-local CbFw2 = CbFw[2]
-local E = setmetatable({}, {
-    GetCurrentBlade = function()
-        local p13 = CbFw2.activeController
-        local ret = p13.blades[1]
-        if not ret then return end
-        while ret.Parent~=game.Players.LocalPlayer.Character do ret=ret.Parent end
-        return ret
-    end,
+local v1 = game.Players.LocalPlayer
+local v2 = debug.getupvalues(require(v1.PlayerScripts.CombatFramework))
+local v3 = v2[2]
+G_1_ = function()
+	local v4 = v3.activeController
+	local v5 = v4.blades[1]
+	if not v5 then
+		return
+	end
+	while v5.Parent ~= game.Players.LocalPlayer.Character do
+		v5 = v5.Parent
+	end
+	return v5
+end
 
-    attack = function()
-        local AC = CbFw2.activeController
-        for i = 1, 1 do 
-            local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(
-                plr.Character,
-                {plr.Character.HumanoidRootPart},
-                60
-            )
-            local cac = {}
-            local hash = {}
-            for k, v in pairs(bladehit) do
-                if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
-                    table.insert(cac, v.Parent.HumanoidRootPart)
-                    hash[v.Parent] = true
-                end
-            end
-            bladehit = cac
-            if #bladehit > 0 then
-                local u8 = debug.getupvalue(AC.attack, 5)
-                local u9 = debug.getupvalue(AC.attack, 6)
-                local u7 = debug.getupvalue(AC.attack, 4)
-                local u10 = debug.getupvalue(AC.attack, 7)
-                local u12 = (u8 * 798405 + u7 * 727595) % u9
-                local u13 = u7 * 798405
-                (function()
-                    u12 = (u12 * u9 + u13) % 1099511627776
-                    u8 = math.floor(u12 / u9)
-                    u7 = u12 - u8 * u9
-                end)()
-                u10 = u10 + 1
-                debug.setupvalue(AC.attack, 5, u8)
-                debug.setupvalue(AC.attack, 6, u9)
-                debug.setupvalue(AC.attack, 4, u7)
-                debug.setupvalue(AC.attack, 7, u10)
-                pcall(function()
-                    for k, v in pairs(AC.animator.anims.basic) do
-                        v:Play()
-                    end                  
-                end)
-                if plr.Character:FindFirstChildOfClass("Tool") and AC.blades and AC.blades[1] then 
-                    game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetCurrentBlade()))
-                    game.ReplicatedStorage.Remotes.Validator:FireServer(math.floor(u12 / 1099511627776 * 16777215), u10)
-                    game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, 4, "") 
-                end
-            end
-        end
-    end,
-        
-    __tostring = function()
-        return "The table has locked!"
-    end
-})
+G_2_ = function()
+	local v6 = v3.activeController
+	for v7 = 1, 1 do
+		local v8 = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(
+            v1.Character, {
+			v1.Character.HumanoidRootPart
+		}, 60)
+		local v9 = {}
+		local v10 = {}
+		for v11, v12 in pairs(v8) do
+			if v12.Parent:FindFirstChild("HumanoidRootPart") and not v10[v12.Parent] then
+				table.insert(v9, v12.Parent.HumanoidRootPart)
+				v10[v12.Parent] = true
+			end
+		end
+		v8 = v9
+		if # v8 > 0 then
+			local v13 = debug.getupvalue(v6.attack, 5)
+			local v14 = debug.getupvalue(v6.attack, 6)
+			local v15 = debug.getupvalue(v6.attack, 4)
+			local v16 = debug.getupvalue(v6.attack, 7)
+			local v17 = (v13 * 798405 + v15 * 727595) % v14
+			local v18 = v15 * 798405
+			(function()
+				v17 = (v17 * v14 + v18) % 1099511627776
+				v13 = math.floor(v17 / v14)
+				v15 = v17 - v13 * v14
+			end)()
+			v16 = v16 + 1
+			debug.setupvalue(v6.attack, 5, v13)
+			debug.setupvalue(v6.attack, 6, v14)
+			debug.setupvalue(v6.attack, 4, v15)
+			debug.setupvalue(v6.attack, 7, v16)
+			pcall(function()
+				for v19, v20 in pairs(v6.animator.anims.basic) do
+					v20:Play()
+				end
+			end)
+			if v1.Character:FindFirstChildOfClass("Tool") and v6.blades and v6.blades[1] then
+				game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange", tostring(G_1_()))
+				game.ReplicatedStorage.Remotes.Validator:FireServer(math.floor(v17 / 1099511627776 * 16777215), v16)
+				game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", v8, 4, "")
+			end
+		end
+	end
+end
 
-return E
+coroutine.wrap(function()
+	while task.wait() do
+		if getgenv().Config.FastAttack then
+			coroutine.resume(coroutine.create(G_2_))
+		end
+	end
+end)()
